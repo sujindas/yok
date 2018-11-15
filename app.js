08e428d118a488c27abc9cc2196bd01bc0a634ee
sujindas <45063777+sujindas@users.noreply.github.com>
2018-11-15 16:41:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-web: node app.js
-npm init
-npm install express --save
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+app.post('/webhook', (req, res) => res.sendStatus(200))
+app.listen(port)
